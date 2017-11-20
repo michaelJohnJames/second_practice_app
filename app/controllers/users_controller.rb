69d9b70e5272e4c84fc5292e3972d6dec5555f23
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-19 21:15:53 -0500

    change show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,7 @@ class UsersController < ApplicationController
 
 
   def show
-    binding.pry
-    @user = User.find_by_id(:user_id)
-
+    @user = User.find_by_id(user.id)
     @user.save
     session[:user_id] = @user.id
 
